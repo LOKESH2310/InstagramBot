312717b4f988ab2f5b6a9a7a1e12a2bd7b683296
LOKESH-LOKI-23 <73383730+LOKESH-LOKI-23@users.noreply.github.com>
2021-02-05 20:31:36 +0530

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -1,13 +1,13 @@
-from backend import login, get_unfollowers, driver, followers, following
+from backend import *
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
-driver = driver()
+driver = webdriver.Chrome()
 
 
 from tkinter import *
 w = Tk()
 w.geometry("800x620")
-w.title("Insta:)Bot")
+w.title("I n s t a :) B o t")
 bg = PhotoImage(file = "bg.png")
 label1 = Label( w  , image = bg)
 label1.place(x = 0, y = 0)
@@ -19,8 +19,18 @@ def about():
     bg = PhotoImage(file="bg.png")
     label1 = Label(w2 , image=bg)
     label1.place(x=0, y=0)
-    listbox = Listbox(w2 , height = 20 , width=35, font=("Helvetica", 12))
+    listbox = Listbox(w2 , height = 18 , width=40, font=("Helvetica", 12))
     listbox.place(x=20, y=20)
+    listbox.insert(1 , "This is an easy to use Instagram bot that can be")
+    listbox.insert(2,  "used by public accounts to find their unfollowers")
+    listbox.insert(3 , "(people who you are following but they do not")
+    listbox.insert(4,  "follow you back), followers and following list." , "\n")
+    listbox.insert(5 , "what else can this bot do?" , "\n")
+    listbox.insert(6 , "-send an automated message to a set of users")
+    listbox.insert(7 , "-like or comment on posts in a particular hashtag")
+    listbox.insert(8 , "to increase your reach you enter your username and")
+    listbox.insert(9 ,"password press the login button so the bot ")
+    listbox.insert(10 , "can start working!")
     w2.mainloop()
 
 def win2():
@@ -30,18 +40,55 @@ def win2():
     bg = PhotoImage(file="bg.png")
     label1 = Label(w2 , image=bg)
     label1.place(x=0, y=0)
-    Text(w2 , height = 15 , width = 32).place(x=50 , y = 100)
-    Checkbutton(w2 , text= "followers").place(x=55 , y = 40)
-    Checkbutton(w2, text="following").place(x=180, y=40)
+    txt =Text(w2 , height = 15 , width = 42 )
+    txt.place(x=30 , y = 90)
+    cb1 = IntVar()
+    cb2 = IntVar()
+    cb3 = IntVar()
+    Checkbutton(w2 , text= "Followers" , bg = "#b1eff2" , variable = cb1 ).place(x=55 , y = 40)
+    Checkbutton(w2, text="Following" , bg = "#b1eff2" , variable = cb2 ).place(x=150, y=40)
+    Checkbutton(w2, text="Un-Followers", bg="#b1eff2", variable=cb3).place(x=250, y=40)
+    def dmsg():
+        username = id.get()
+        message = txt.get("1.0",END)
+        if cb1.get() == 1 :
+            user = followers(driver, username)
+        elif cb2.get() == 1:
+            user = following(driver, username)
+        elif cb3.get() == 1:
+            get_unfollowers(driver, username)
+        automate_message(driver, username, message, user)
+    #user = cb2.get()
+    button5 = Button(w2, text='Send=>', width=10, activebackground="magenta2", bd=3, bg="plum1", font="Lato" , command = dmsg )
+    button5.place(x=220, y=350)
     w2.mainloop()
 
 
+
+
+def win3():
+    w2 = Toplevel()
+    w2.geometry("400x250")
+    w2.title("Like ")
+    bg = PhotoImage(file="bg.png")
+    label1 = Label(w2 , image=bg)
+    label1.place(x=0, y=0)
+    Label(w2, text='Add a hashtag to be Liked', font=("Helvetica", 15), bg="#b1eff2").place(x=30, y=20)
+    txt =Text(w2 , height = 5 , width = 42 )
+    txt.place(x=30 , y = 90)
+    def like():
+        hashtag = txt.get("1.0", END)
+        like_posts(driver, hashtag)
+    button5 = Button(w2, text='Like Us', width=10, activebackground="magenta2", bd=3, bg="plum1", font="Lato" , command = like)
+    button5.place(x=250, y=200)
+
+    w2.mainloop()
+
 #labels and entries
-lab1 = Label(w, text='Instagram bot' , font=("Helvetica", 32) , bg = "#b1eff2").place(x=250,y=20)
+Label(w, text='I.n.s.t.a :) b.o.t' , font=("Helvetica", 32) , bg = "#b1eff2").place(x=250,y=20)
 Label(w, text=' INSTAGRAM ID : ' ,font=("Helvetica", 12) , bg = "#b1eff2").place(x=230,y=100)
-Label(w, text='PASSWORD :' ,font=("Helvetica", 12) ,  bg ="#b1eff2" ).place(x=250,y=140)
+Label(w, text='PASSWORD :' ,font=("Helvetica", 12) ,  bg ="#b1eff2" ).place(x=255,y=140)
 id = StringVar()
-id.set("Login id")
 pas = StringVar()
 e1 = Entry(w, textvariable= id , font=("Helvetica", 12) ).place(x=370,y=100)
 e2 = Entry(w, textvariable = pas , font=("Helvetica", 12) , show = "*").place(x=370,y=140)
@@ -78,6 +125,7 @@ def unfol():
         n = i+1
         listbox.insert(END , (n ,":" ,unfollowers[i]) , "\n")
 
+
 def foll():
     username = id.get()
     listbox.delete(0, END)
@@ -92,6 +140,10 @@ def foling():
     for i in range(len(followin)):
         n = i+1
         listbox.insert(END , (n ,":" ,followin[i] ) , "\n")
+def logout1():
+    if logout(driver):
+        print("loged out")
+
 
 
 
@@ -101,16 +153,23 @@ def foling():
 
 #buttons
 
-button1 = Button(w, text='Log In', width=15 ,activebackground = "magenta2" , bd= 3 , bg = "LightSkyBlue1" , font = "Lato" , command = log)
+button1 = Button(w, text='Log In', width=15 ,activebackground = "magenta2" , bd= 3 , bg = "LightSkyBlue1" , font = "Lato" , command = log )
+
 button1.place(x= 310 , y = 180)
 button2 = Button(w, text='Un-followers', width=15 , activebackground = "magenta2" , bd= 3 , bg = "plum1", font = "Lato" , command = unfol )
 button2.place(x= 90 , y= 250 )
 button3 = Button(w, text='Followers', width=15, activebackground = "magenta2" , bd= 3 , bg = "plum1" , font = "Lato" , command = foll)
-button3.place(x= 240 , y= 250 )
+button3.place(x= 320 , y= 250 )
 button4 = Button(w, text='Following', width=15 , activebackground = "magenta2" , bd= 3 , bg = "plum1" , font = "Lato" , command = foling )
-button4.place(x= 390 , y= 250 )
-button4 = Button(w, text='new', width=15 , activebackground = "magenta2" , bd= 3 , bg = "plum1" , font = "Lato" , command = win2)
 button4.place(x= 540 , y= 250 )
+button4 = Button(w, text='DM', width=15 , activebackground = "magenta2" , bd= 3 , bg = "plum1" , font = "Lato" , command = win2)
+button4.place(x= 90 , y= 510 )
+button4 = Button(w, text='Log out', width=15 , activebackground = "magenta2" , bd= 3 , bg = "plum1" , font = "Lato" , command = logout1 )
+button4.place(x= 540 , y= 510 )
+button6 = Button(w, text='Like Me', width=15 , activebackground = "magenta2" , bd= 3 , bg = "plum1" , font = "Lato" , command = win3 )
+button6.place(x= 320 , y= 510 )
+
+
 
 
 
